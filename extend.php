41f1121550a2701411defb4a6f10f8cb979d8d2e
daocatt <daocatt@users.noreply.github.com>
2024-03-03 17:53:05 +0800

    support darkmode

--- extend.php
+++ extend.php
@@ -8,6 +8,7 @@ use Flarum\Api\Controller\ShowDiscussionController;
 return [
   (new Extend\Frontend('forum'))
       ->js(__DIR__.'/js/dist/forum.js')
+      ->css(__DIR__.'/less/_variables.less')
       ->css(__DIR__.'/less/forum.less'),
 
   // new Extend\Locales(__DIR__ . '/locale'),
